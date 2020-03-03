b2677b641cd325971d59d3db186c5d16dda80051
ernestomelchor <nurturelove88@gmail.com>
2020-03-02 21:06:27 -0600

    added code to app

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,22 @@
 class Api::ContactsController < ApplicationController
+  def first_contact_action
+    @contact = Contact.first[:first_name]
+    render "first_contact.json.jb"
+  end
+
+  def all_contacts_by_first_name_action
+    @contacts = Contact.all
+    index = 0
+    @all_contacts_by_first_name = []
+    while index < @contacts.length
+      @all_contacts_by_first_name << @contacts[index][:first_name]
+      index += 1
+    end
+    render "all_contacts_by_first_name.json.jb"
+  end
+
+  def all_contacts_action
+    @contacts = Contact.all
+    render "all_contacts.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/first_contact" => "contacts#first_contact_action"
+    get "/all_contacts_by_first_name" => "contacts#all_contacts_by_first_name_action"
+    get "/all_contacts" => "contacts#all_contacts_action"
+  end
 end
