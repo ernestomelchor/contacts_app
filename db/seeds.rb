1b74455a604085dfba867fdae332fbfe98929aea
ernestomelchor <nurturelove88@gmail.com>
2020-03-02 23:33:42 -0600

    updated contacts in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+contact = Contact.new({ first_name: "Sonya", last_name: "Blade", email: "sonyablade@example.com", phone_number: "1234567890" })
+contact.save
+contact = Contact.new({ first_name: "Sub", last_name: "Zero", email: "subzero@example.com", phone_number: "9876543210" })
+contact.save
+contact.savecontact = Contact.new({ first_name: "Liu", last_name: "Kang", email: "liukang@example.com", phone_number: "7771234567" })
+contact.save
+create db: seed
